c5ba7ce1e439b8e61d9ac8ac380d5176c38f2506
OmarBahmad <omarbahmad@gmail.com>
2021-03-09 18:05:04 -0300

    Create Dump20210309 (2).sql

---
+++ Dump20210309 (2).sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cadastro
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `idcurso` int NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `descrição` text,
+  `carga` int unsigned DEFAULT NULL,
+  `totaulas` int DEFAULT NULL,
+  `ano` year DEFAULT '2020',
+  PRIMARY KEY (`idcurso`),
+  UNIQUE KEY `nome` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'HTML5','CURSO DE HTML5',40,37,2014),(2,'ALGORITMOS','LOGICA DE PROGRAMAÇÃO',20,15,2014),(3,'PHOTOSHOP','DICAS DE PHOTOSHOP CC',10,8,2014),(4,'PHP','CURSO DE PHP PARA INICIANTES',40,20,2010),(5,'JAVA','INTRODUÇÃO A LINGUAGEM JAVA',40,29,2015),(6,'MYSQL','BANCO DE DADOS MYSQL',30,15,2016),(7,'WORD','CURSO COMPLETO DE WORD',40,30,2016),(9,'COZINHA ARABE','APRENDA A FAZER KIBE',0,30,2018),(10,'YOUTUBER','GERAR POLEMICA E GANHAR INSCRITOS',800,2,2050);
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gafanhotos`
+--
+
+DROP TABLE IF EXISTS `gafanhotos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gafanhotos` (
+  `id` int DEFAULT NULL,
+  `nome` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gafanhotos`
+--
+
+LOCK TABLES `gafanhotos` WRITE;
+/*!40000 ALTER TABLE `gafanhotos` DISABLE KEYS */;
+INSERT INTO `gafanhotos` VALUES (1,'Maria'),(2,'Joao'),(3,'Pedro');
+/*!40000 ALTER TABLE `gafanhotos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-09 12:19:49
